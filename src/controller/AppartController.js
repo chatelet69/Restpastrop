00c84f764a8a49b32fc7b44fe7fbca71d2baee9d
SpyKo_ <mathfremiot@gmail.com>
2023-12-04 23:06:12 +0100

    Appart Controller for creating appart

--- src/controller/AppartController.js
+++ src/controller/AppartController.js
@@ -1,12 +1,24 @@
 const AppartService     = require("../services/AppartService");
 
 class AppartController{
-    service;
     constructor(){
         this.service = new AppartService;
     }
 
-    postAppart(req, res){
-        
-    }
+    async postAppart(req, res){
+        try {
+            const body = req.body;
+            console.log(body);
+            let result = await this.service.createAppart(body, res);
+            if (result) {
+                res.status(200);
+                res.json({message: "success", jwt: "A CHANGER"});
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({error: "Error during creating appartments"});
+        }
+    }       
 }
+
+module.exports = AppartController;
