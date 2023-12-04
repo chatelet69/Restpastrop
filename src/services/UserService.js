54608224dd85d1409bf3ad28ce6902ebb2427eb5
L4p1s <124094769+PommePain@users.noreply.github.com>
2023-12-05 00:03:39 +0100

    User login & user register Merge avancement vers dev (#6)
    * Advance users
    
    * Update voir desc
    
    Update index
    Update global
    Update package json
    
    * Update fix
    
    * Update mvc
    
    Update controller, repo, user, userService
    
    * Ajouts middlewares log & auth
    
    Auth middleware, authorization & admin
    
    * Update index / global & packages
    
    modif index, global & packages

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,16 +1,39 @@
 const User              = require("../model/User");
-const UserRepository  = require("../repository/UserRepository");
+const UserRepository    = require("../repository/UserRepository");
+const sha512            = require('js-sha512');
+const jwt               = require("jsonwebtoken");
+const secret            = require("../../config.json").secretJwt;
 
 class UserService {
     constructor() {
         this.userRepository = new UserRepository();
     }
-
-    authLoginService(username, password) {
+  
+    async authLoginService(username, password) {
         try {
-            const checkLogin = this.userRepository.checkLogin(username, password);
+            let authToken = false;
+            const hash = sha512(password);
+            const checkLogin = await this.userRepository.checkLogin(username, hash);
+            if (checkLogin) {
+                authToken = jwt.sign({data: username, role: checkLogin.rank, userId: checkLogin.id}, secret, { expiresIn: "1h" });
+                let check = await this.userRepository.setNewKey(checkLogin.id, authToken);
+                if (check.affectedRows == 0) authToken = false;
+            }
+            return (authToken) ? authToken : false;
         } catch (error) {
             console.log(error);
+            return false;
+        }
+    }
+
+    async myUserService(userId) {
+        try {
+            let userData = false;
+            if (userId != 0) userData = await this.userRepository.getUserById(userId);
+            return userData;
+        } catch (error) {
+            console.log(error);
+            return false;
         }
     }
 }
