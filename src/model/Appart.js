3bf3a1a1e6202d8f9105a93ca7676de3b148f0f0
SpyKo_ <mathfremiot@gmail.com>
2023-12-04 17:23:01 +0100

    Update Appart model

--- src/model/Appart.js
+++ src/model/Appart.js
@@ -1,13 +1,22 @@
 class Appart {
-    constructor(id, title, ...any) {
+    constructor(id, title, wifi = null, parking = null, childAdapted = null, balcon = null, smoker = null, ...any) {
         this.id = id;
         this.title = title;
+        spec = array();
+        this.spec['wifi'] = wifi;
+        this.spec['parking'] = parking;
+        this.spec['childAdapted'] = childAdapted;
+        this.spec['balcon'] = balcon;
+        this.spec['smoker'] = smoker;
+
         for (const arg of any) this.arg = arg;
     };
 
     // Getters
 
     getId() { return this.id; }
+
+    getSpec() {return this.spec;}
 
     getTitle() { return this.title; }
 
@@ -19,9 +28,15 @@ class Appart {
 
     getAddress() { return this.address; }
 
+    getRooms() {return this.nbRooms;}
+
+    getPersonCount() {return this.PersonCount;}
+
     // Setters 
 
     setId(newId) { this.id = newId; }
+
+    setSpec(...any) { for (const arg of any) this.spec[arg] = arg;}
 
     setTitle(newTitle) { this.title = newTitle; }
 
@@ -32,6 +47,11 @@ class Appart {
     setOwner(newOwner) { this.owner = newOwner; }
 
     setAddress(newAddress) { this.address = newAddress; }
+
+    setRooms(nbRooms) { this.nbRooms = nbRooms;}
+
+    setPersonCount(PersonCount) { this.PersonCount = PersonCount;}
+
 }
 
 module.exports = new Appart();
