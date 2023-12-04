d6570ecea4b0e798c30231d839ae159811acd90c
PommePain <124094769+PommePain@users.noreply.github.com>
2023-12-04 15:31:39 +0100

    Initial Modules see desc
    Ajout initial d'un controller, model et repository

---
+++ src/model/Appart.js
@@ -0,0 +1,44 @@
+class Appart {
+    id;
+    owner;
+    title;
+    address;
+    price;
+    status;
+
+    constructor(id, title, ...any) {
+        this.id = id;
+        this.title = title;
+        for (const arg of any) this.arg = arg;
+    };
+
+    // Getters
+
+    getId() { return this.id; }
+
+    getTitle() { return this.title; }
+
+    getStatus() { return this.status; }
+
+    getPrice() { return this.price; }
+
+    getOwner() { return this.owner; }
+
+    getAddress() { return this.address; }
+
+    // Setters 
+
+    setId(newId) { this.id = newId; }
+
+    setTitle(newTitle) { this.title = newTitle; }
+
+    setStatus(newStatus) { this.status = newStatus; }
+
+    setPrice(newPrice) { this.price = newPrice; }
+
+    setOwner(newOwner) { this.owner = newOwner; }
+
+    setAddress(newAddress) { this.address = newAddress; }
+}
+
+module.exports = new Appart();
