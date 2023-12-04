b51a8ef451a04fe8efe2a5357e2e101e65ed3e2d
PommePain <124094769+PommePain@users.noreply.github.com>
2023-12-04 15:30:22 +0100

    Create global.js

---
+++ src/routes/global.js
@@ -0,0 +1,10 @@
+const express           = require('express');
+const router            = express.Router();
+
+router.get('/', (req, res) => {
+    console.log("Requête reçue sur la route", req.route.path)
+    res.status(200);
+    res.json({message: "Welcome to the Api, see documentation here"});
+});
+
+module.exports = router;
