569dfa706e1e41a4e17caec6b98bdab808688d7d
L4p1s <124094769+PommePain@users.noreply.github.com>
2023-12-04 23:58:55 +0100

    Update mvc
    Update controller, repo, user, userService

--- src/repository/UserRepository.js
+++ src/repository/UserRepository.js
@@ -8,12 +8,32 @@ class UserRepository {
         this.db = new DatabaseConnection();
     }
 
+    getUserById(userId) {
+        const sqlQuery = "SELECT username, name, lastname, rank, email FROM users WHERE id = ?";
+        return new Promise((resolve, reject) => {
+            this.db.query(sqlQuery, [userId], (error, result) => {
+                if (error) throw error;
+                resolve(result[0]);
+            });
+        });
+    }
+
     checkLogin(username, password) {
-        const sqlQuery = "SELECT username, name, lastname, rank, email FROM users" 
+        const sqlQuery = "SELECT id, username, name, lastname, rank, email FROM users" 
             + " WHERE username = ? AND password = ?";
         return new Promise ((resolve, reject) => {
             this.db.query(sqlQuery, [username, password], (error, result) => {
-                if (error) reject(error);
+                if (error) throw (error);
+                resolve(result[0]);
+            });
+        });
+    }
+
+    setNewKey(userId, jwt) {
+        const sqlQuery = "UPDATE users set jwt = ? WHERE id = ?";
+        return new Promise ((resolve, reject) => {
+            this.db.query(sqlQuery, [jwt, userId], (error, result) => {
+                if (error) throw (error);
                 resolve(result);
             });
         });
