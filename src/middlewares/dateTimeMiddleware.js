b7ff6aa307d9b73475b75bf1740d1adcd42ba324
L4p1s <124094769+PommePain@users.noreply.github.com>
2023-12-07 00:31:10 +0100

    Ajout dateTimeMiddleware

---
+++ src/middlewares/dateTimeMiddleware.js
@@ -0,0 +1,23 @@
+const moment = require("moment");
+
+const dateTimeMiddleware = (req, res, next) => {
+    let formatDates = {
+        starDate: req.body.startDate,
+        endDate: req.body.endDate
+    };
+
+    for (const date in formatDates) {
+        // Vérifie si une heure est indiquée dans les dates envoyées
+        if (formatDates[date].includes(':')) formatDates[date] = moment(formatDates[date], "YYYY-MM-DD HH:mm:ss");
+        // Afin d'adapter le format des dates 
+        else formatDates[date] = moment(formatDates[date], "YYYY-MM-DD");
+        formatDates[date] = formatDates[date].format("YYYY-MM-DD HH:mm:ss");
+    }
+
+    // On réaffecte les dates formatées
+    req.body.startDate = formatDates.starDate;
+    req.body.endDate = formatDates.endDate;
+    next();
+}
+
+module.exports = dateTimeMiddleware;
