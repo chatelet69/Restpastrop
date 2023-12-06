59bea8696ef3ef52a95eec7e91b7a7e39be6a3d4
L4p1s <124094769+PommePain@users.noreply.github.com>
2023-12-07 00:31:18 +0100

    Update checkIfadmin

--- src/middlewares/checkIfAdmin.js
+++ src/middlewares/checkIfAdmin.js
@@ -1,6 +1,7 @@
 const checkIfAdmin = (req, res, next) => {
     const reqUser = req.user;
     if (!reqUser || reqUser.role !== "admin") req.user.isAdmin = false;
+    else if (reqUser && reqUser.role === "admin") req.user.isAdmin = true;
     next();
 };
 
