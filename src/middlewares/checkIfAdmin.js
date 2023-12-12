1a59caf4c00d25571459a882b721c04e046c74b4
L4p1s <124094769+PommePain@users.noreply.github.com>
2023-12-12 01:38:39 +0100

    Fix & edit midlewares et user service

--- src/middlewares/checkIfAdmin.js
+++ src/middlewares/checkIfAdmin.js
@@ -1,7 +1,7 @@
 const checkIfAdmin = (req, res, next) => {
     const reqUser = req.user;
-    if (!reqUser || reqUser.role !== "admin") req.user.isAdmin = false;
-    else if (reqUser && reqUser.role === "admin") req.user.isAdmin = true;
+    if (!reqUser || reqUser.rank !== "admin") req.user.isAdmin = false;
+    else if (reqUser && reqUser.rank === "admin") req.user.isAdmin = true;
     next();
 };
 
